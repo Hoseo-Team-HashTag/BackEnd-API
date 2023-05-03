d7f305ca80ceb88eb9e6a83bb93a48d20c352c1e
ParkSeongMoon <psmoon7304@naver.com>
2023-05-04 00:58:54 +0900

    Axios를 통한 Front/Back API 서버 구성

--- router/accounts.js
+++ router/accounts.js
@@ -1,8 +1,16 @@
 const express = require("express")
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    console.log("Accounts.js 접근")
+router.post('/', (req, res) => {
+    console.log("Accounts.js 접근 Data : " + req.body.content + "/" + req.body.d);
+    return res.status(200).json({
+        msg : "테스트",
+        data1 : req.body.content
+    })
+})
+
+router.post('/hello',(req,res)=> {
+    console.log("Hello")
 })
 
 module.exports = router;
--- router/index.js
+++ router/index.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send("Test Router")
+router.post('/', (req, res) => {
+    console.log("값 테스트 : " + req.body.number)
+    return res.status(200).json({
+        message:"테스트"
+    })
 })
 
 module.exports = router;
