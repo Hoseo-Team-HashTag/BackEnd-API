05aa9c807d60920ee4c044aee95c897e67d7ac5b
Your Name <you@example.com>
2023-06-27 00:33:39 +0900

    calendar version.2

--- router/calendar.js
+++ router/calendar.js
@@ -5,8 +5,13 @@ const router = express.Router()
 router.post('/Calendar', (req, res) => {
     const { user_id, title, content, calendar_date, calendar_time } = req.body; // id, 제목, 내용, 날짜, 시간을 받아옴
 
-    const query = 'INSERT INTO calendar (user_id, title, content, calendar_date, calendar_time) VALUES (?, ?, ?, ?, ?)';
-    const values = [user_id, title, content, calendar_date, calendar_time];
+    if(!user_id || !title || !content || !calendar_date){
+        res.status(400).json({message: 'Required fields are missing.'});
+        return;
+    }// id, 제목, 내용, 날짜 중 하나라도 널값인 경우 오류 반환
+
+    const query = 'INSERT INTO calendar (user_id, title, content, calendar_date, calendar_time) VALUES (?, ?, ?, ?, ?)'; //query문
+    const values = [user_id, title, content, calendar_date, calendar_time]; //프론트엔드에서 전달받은 값들을 배열로 저장
 
     mysql.pool.query(query, values, (error, results)=> {
         if(error) {
@@ -15,6 +20,6 @@ router.post('/Calendar', (req, res) => {
         } else {
             res.status(200).json({ message: 'Data saved successfully. '});
         }
-    });
+    });//pool.query는 pool.getConnection()을 내부적으로 처리하고 연결을 가져오고 쿼리를 실행한뒤 연결을 자동으로 반환.
 });
 module.exports = router;
